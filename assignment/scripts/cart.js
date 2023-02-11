655b427c25e88d240758062d1b892d4b418c47aa
Ia Vue <iavue00@gmail.com>
2023-02-10 19:46:47 -0600

    finished first function

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -3,3 +3,14 @@ console.log('***** Cart Functions *****');
 // We want to see how you are testing your code!!!
 
 let basket = [];
+
+function addItem( item ){
+    console.log('testing # of items:', item);
+    if (basket.length > 0){
+        return true;
+    }
+    return false;
+}
+console.log(`Basket is: ${basket}`);
+console.log('Adding milk - should be true:', addItem(basket.push('milk')));
+console.log(`Basket is now: ${basket}`);
